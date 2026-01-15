93ce1ef396c0619615db2cc2a6ab1720ed4d4a99
Weesley0420 <gomez.paulwesley@gmail.com>
2026-01-15 16:38:12 +0000

    asf

--- exp1s.c
+++ exp1s.c
@@ -1,27 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <stdio.h>      // For printf()
+#include <stdlib.h>     // For exit()
+#include <unistd.h>     // For fork(), execlp(), getpid()
+#include <sys/wait.h>   // For wait()
 
 int main(int argc, char *arg[])
 {
-    int pid;
-    pid = fork();
+    int pid;            // Variable to store the process ID
 
-    if (pid < 0)
+    pid = fork();       // Create a new process (child process)
+
+    if (pid < 0)        // If fork() returns a negative value
     {
-        printf("fork failed\n");
-        exit(1);
+        printf("fork failed\n"); // Fork failed
+        exit(1);        // Exit the program with error
     }
-    else if (pid == 0)
+    else if (pid == 0)  // If fork() returns 0, this is the child process
     {
-        execlp("whoami", "1s", NULL);
-        exit(0);
+        execlp("whoami", "1s", NULL); // Replace child process with "whoami" command
+        exit(0);        // Exit child process after execution
     }
-    else
+    else                // If fork() returns a positive value, this is the parent process
     {
-        printf("\nProcess ID is: %d\n", getpid());
-        wait(NULL);
-        exit(0);
+        printf("\nProcess ID is: %d\n", getpid()); // Print parent process ID
+        wait(NULL);     // Parent waits for child process to finish
+        exit(0);        // Exit parent process
     }
 }
